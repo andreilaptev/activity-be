f87d48990cd65d7c319f20dc94e78044b9a91aa6
Andrei <andrey.laptev@outlook.com>
2020-06-08 21:05:32 -0400

    some fixes

--- Activities/Controllers/ActivitiesController.cs
+++ Activities/Controllers/ActivitiesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Activities.Data;
-using ActivitySignUp.Models;
+using Activities.Models;
 
 namespace Activities.Controllers
 {
--- Activities/Data/ActivitiesContext.cs
+++ Activities/Data/ActivitiesContext.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using ActivitySignUp.Models;
 using Activities.Models;
 
 namespace Activities.Data
@@ -15,7 +14,7 @@ namespace Activities.Data
         {
         }
 
-        public DbSet<ActivitySignUp.Models.Activity> Activity { get; set; }
+        public DbSet<Activities.Models.Activity> Activity { get; set; }
 
         public DbSet<Activities.Models.User> User { get; set; }
 
